df1e5f5e42187c80c0e1768ca5f28fed05a321c4
Pawel Maczuga <pmaczuga97@gmail.com>
2022-04-06 13:08:25 +0100

    Added vertex ids, vertex uve and interior's ids to export_inp

--- src/io.jl
+++ src/io.jl
@@ -6,20 +6,10 @@ function export_inp(g, filename)
         t_map = Dict()
         fun_map = Dict()
         counter = 0
-        list_nv = collect(normal_vertices(g))
-        list_hv = collect(MeshGraphs.hanging_nodes(g))
+        list_nv = collect(vertices_except_type(g, INTERIOR))
         list_interiors = collect(interiors(g))
-        write(io, @sprintf("%d %d 0 0 0\n", length(list_nv) + length(list_hv), length(list_interiors)))
+        write(io, @sprintf("%d %d 4 1 0\n", length(list_nv), length(list_interiors)))
         for v in list_nv
-            x, y, z = xyz(g, v)
-            counter = counter + 1
-            write(io, @sprintf("%d %f %f %f\n", counter, x, y, z))
-            t_map[v] = v_id
-            v_id += 1
-        end
-
-        # TODO remove
-        for v in list_hv
             x, y, z = xyz(g, v)
             counter = counter + 1
             write(io, @sprintf("%d %f %f %f\n", counter, x, y, z))
@@ -34,5 +24,21 @@ function export_inp(g, filename)
             counter = counter + 1
             write(io, @sprintf("%d 0 tri %d %d %d\n", counter, t_map[v1], t_map[v2], t_map[v3]))
         end
+
+        counter = 0
+        write(io, "2 1 3\nvertex_id,nunits\nuve,degree\n")
+        for i in list_nv
+            counter += 1
+            u,v,e = uve(g, i)
+            write(io, "$counter $i $u $v $e\n")
+        end
+
+        counter = 0
+        write(io, "1 1\ninterior_id,nunits\n")
+        for i in list_interiors
+            counter += 1
+            write(io, "$counter $i\n")
+        end
+
     end
 end
